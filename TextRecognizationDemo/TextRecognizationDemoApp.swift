93b9b8b249df2f6a68c374db5572854fcc931cd7
Joynal Abedin <44470728+Joynal279@users.noreply.github.com>
2023-02-18 00:45:02 +0600

    Initial Commit

---
+++ TextRecognizationDemo/TextRecognizationDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextRecognizationDemoApp.swift
+//  TextRecognizationDemo
+//
+//  Created by Joynal Abedin on 18/2/23.
+//
+
+import SwiftUI
+
+@main
+struct TextRecognizationDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
